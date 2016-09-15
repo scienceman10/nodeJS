19f0bc66f3d9951c78bc39110cceeb0db9b906c7
Benjamin <none@none.com>
2016-09-15 14:58:19 -0600

    more stuff

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+// app.disable("x-powered-by").create({dafaultLayout:"main"});
 const handlebars = require('express-handlebars');
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", handlebars);
+//more imports go here
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+
+
 
 /*app.get('/', function (req, res) {
   res.send('<h1 style="text-align:center">my website</h1>');
