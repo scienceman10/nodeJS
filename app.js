22578bdb2b57e849aa250fe5d72c1eb3b7e0d522
Marek Newton <marek.s.newton@gmail.com>
2016-09-18 17:10:45 -0600

    file path setup...

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ const app = express();
 // app.disable("x-powered-by").create({dafaultLayout:"main"});
 const handlebars = require('express-handlebars').create({defaultLayout:"main"});
 app.engine("handlebars", handlebars.engine);
-app.set("view engine", handlebars);
+app.set("view engine", "handlebars");
+app.use(require("body-parser").urlencoded({extended:true}));
+const formidable = require('formidable');
 //more imports go here
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 
-
-
-
 app.get("/", (req, res)=>{
   res.render("home")
+});
+
+app.get("/about", (req, res)=>{
+  res.render("about")
+});
+
+app.use((req, res)=>{
+  res.type("text/html");
+  res.status(404);
+  res.render("404");
+});
+
+app.use((err, req, res, next)=>{
+  console.log(err.stack);
+  res.status(500);
+  res.render("500");
 })
 
 app.listen(3000, function () {
