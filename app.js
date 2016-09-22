7da487b2761b54ee6f0c73656667fab087b92ac4
Benjamin <none@none.com>
2016-09-22 17:19:57 -0600

    file-upload/stuff

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// app.disable("x-powered-by").create({dafaultLayout:"main"});
+//app.disable("x-powered-by").create({dafaultLayout:"main"});
 const handlebars = require('express-handlebars').create({defaultLayout:"main"});
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
@@ -13,15 +13,15 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res)=>{
-  res.render("home")
+  res.render("home", {title:"home"})
 });
 
 app.get("/about", (req, res)=>{
-  res.render("about");
+  res.render("about", {title:"about"});
 });
 
 app.get("/contact", (req, res)=>{
-  res.render('contact', { csrf: 'CSRF token here' });
+  res.render('contact', { csrf: 'CSRF token here', title:"contact us" });
 });
 
 app.get("/thankyou", (req, res)=>{
@@ -35,11 +35,28 @@ app.post('/process', function(req, res){
   res.redirect(303, '/thankyou');
 });
 
+app.get('/file-upload', (req, res)=>{
+  var now = new Date();
+  res.render('file-upload',{
+    year: now.getFullYear(),
+    month: now.getMonth() });
+});
+
+app.post("/file-upload/:year/:month", (req, res)=>{
+  var form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, file)=>{
+    if (err)
+      return res.redirect(303, "/500");
+    console.log("upload form accepted");
+    console.log(file);
+    res.redirect(303, "/thankyou")
+  })
+})
 
 app.use((req, res)=>{
   res.type("text/html");
   res.status(404);
-  res.render("404");
+  res.render("404", {title:"404 not found", layout:"second"});
 });
 
 app.use((err, req, res, next)=>{
